5c638e02b2a753185217272eb156a6fb7e97cfbd
Talha Riaz <talhariaz324@gmail.com>
2025-05-03 12:32:07 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,8 @@ async function bootstrap() {
       'http://localhost:5173',
       'http://localhost:8080',
       'http://localhost:3000',
-      'https://ubiquitous-meringue-177a4c.netlify.app'
+      'https://ubiquitous-meringue-177a4c.netlify.app',
+      'https://lindo-mart-frontend-taupe.vercel.app'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
